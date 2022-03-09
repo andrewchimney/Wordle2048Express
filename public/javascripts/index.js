5aaf9b263da897ce650ed2abbe10c1d33f61b5d4
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2022-03-08 21:57:08 -0800

    Fresh start

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -15,7 +15,15 @@ function setUpInput(){
 
 function handleInput(e){
     console.log(e.key);
+    tile.style.setProperty("--x", 0);
     setUpInput();
+
 }
+let tile = document.createElement("div");
+tile.classList.add("tile");
+let board = document.getElementById("board");
+board.append(tile);
+
+
 
 
