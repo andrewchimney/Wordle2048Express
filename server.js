f417d0e786477a8b7640cfa0f8be7b8a42e12036
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2022-03-13 15:05:38 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const fs = require("fs");
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
@@ -11,8 +10,4 @@ app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/pages/index.html")
 })
-app.post("/", (req, res) => {
-    console.log(req);
-    // let scoresJSON = JSON.parse(fs.readFileSync("public/scores.json", "utf-8"));
-    // scoresJSON.yeet = "askjdhflasdfka";
-})
+module.exports = app;
