e771a9152c34bf8768e5ee1c09eec739d559432e
andrewchimney <93691894+andrewchimney@users.noreply.github.com>
2022-03-14 11:31:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,4 @@ app.listen(port, () => {
   })
 app.use(express.json({limit:'1mb'}))
 app.use(express.static("public"));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+"/pages/index.html")
-})
 module.exports = app;
